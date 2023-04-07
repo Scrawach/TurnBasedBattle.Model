c3fb4957d631167bb02c38ad68c018fa601037a3
scrawach <scrawach@gmail.com>
2023-04-07 09:50:52 +0300

    added initiative initialize in fighter factory

--- TurnBasedBattle.Model.Core/Factory/FighterFactory.cs
+++ TurnBasedBattle.Model.Core/Factory/FighterFactory.cs
@@ -5,23 +5,26 @@ using TurnBasedBattle.Model.Core.Factory.Abstract;
 
 namespace TurnBasedBattle.Model.Core.Factory
 {
-    public sealed class HumanFactory : IFactory
+    public sealed class FighterFactory : IFactory
     {
         private readonly int _health;
         private readonly int _mana;
         private readonly int _fighting;
+        private readonly int _initiative;
 
-        public HumanFactory(int health, int mana, int fighting)
+        public FighterFactory(int health, int mana, int fighting, int initiative)
         {
             _health = health;
             _mana = mana;
             _fighting = fighting;
+            _initiative = initiative;
         }
 
         public IEntity Create() =>
             new Entity()
                 .Add(new Health(_health))
                 .Add(new Mana(_mana))
-                .Add(new Fighting(_fighting));
+                .Add(new Fighting(_fighting))
+                .Add(new Initiative(0, _initiative));
     }
 }
