a1f4d5d99312cd36fede0d138358b94c5df79e35
scrawach <scrawach@gmail.com>
2023-04-07 09:53:09 +0300

    implemented fighter config for creation

---
+++ TurnBasedBattle.Model.Core/Factory/Configs/FighterConfig.cs
@@ -0,0 +1,10 @@
+namespace TurnBasedBattle.Model.Core.Factory.Configs
+{
+    public sealed class FighterConfig
+    {
+        public int Health { get; set; }
+        public int Mana { get; set; }
+        public int Power { get; set; }
+        public int Initiative { get; set; }
+    }
+}
--- TurnBasedBattle.Model.Core/Factory/FighterFactory.cs
+++ TurnBasedBattle.Model.Core/Factory/FighterFactory.cs
@@ -2,29 +2,22 @@
 using TurnBasedBattle.Model.Core.Entities;
 using TurnBasedBattle.Model.Core.Entities.Abstract;
 using TurnBasedBattle.Model.Core.Factory.Abstract;
+using TurnBasedBattle.Model.Core.Factory.Configs;
 
 namespace TurnBasedBattle.Model.Core.Factory
 {
     public sealed class FighterFactory : IFactory
     {
-        private readonly int _health;
-        private readonly int _mana;
-        private readonly int _fighting;
-        private readonly int _initiative;
+        private readonly FighterConfig _config;
 
-        public FighterFactory(int health, int mana, int fighting, int initiative)
-        {
-            _health = health;
-            _mana = mana;
-            _fighting = fighting;
-            _initiative = initiative;
-        }
+        public FighterFactory(FighterConfig config) =>
+            _config = config;
 
         public IEntity Create() =>
             new Entity()
-                .Add(new Health(_health))
-                .Add(new Mana(_mana))
-                .Add(new Fighting(_fighting))
-                .Add(new Initiative(0, _initiative));
+                .Add(new Health(_config.Health))
+                .Add(new Mana(_config.Mana))
+                .Add(new Fighting(_config.Power))
+                .Add(new Initiative(0, _config.Initiative));
     }
 }
